4693727e3a9d0793db45302e72b508ef4cbe2b90
Kyuka Ibrahim Avong <drkyavoni@live.com>
2024-01-13 10:05:48 +0100

    v0.1.2"

---
+++ console.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+
+import cmd
+
+import models
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+classDict = {"BaseModel": BaseModel, "FileStorage": FileStorage}
+
+
+class HBNBCommand(cmd.Cmd):
+    """HBNBCommand class"""
+
+    prompt = "(hbnb) "
+
+    @classmethod
+    def __checkMissingClassName(cls, arg) -> int:
+        if not arg:
+            print("{}".format("** class name missing **"))
+            return 1
+        return 0
+
+    @classmethod
+    def __get_class(cls, arg):
+        """Returns the class object for the given class name"""
+        argElements = arg.split()
+        objClass: object = None
+        try:
+            if argElements[0] != "BaseModel":
+                print("{}".format("=> ** class doesn't exist **"))
+                objClass = None
+            else:
+                objClass = classDict.get(argElements[0])
+        except KeyError:
+            print("{}".format("** class doesn't exist **"))
+        finally:
+            return objClass
+
+    @classmethod
+    def __checkMissingClassID(cls, arg):
+        argElements = arg.split()
+
+        if len(argElements) < 2:
+            print("{}".format("** instance id missing **"))
+            return 1
+        return 0
+
+    @classmethod
+    def __get_classDetails(cls, arg):
+        argElements = arg.split()
+
+        objKey = "{}.{}".format(argElements[0], argElements[1])
+
+        if objKey not in models.storage.all():
+            print("{}".format("** no instance found **"))
+            return None
+
+        return models.storage.all().get(objKey)
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """Do nothing when an empty line is entered"""
+        pass
+
+    def do_create(self, arg):
+        """
+        Creates a new object based on the given argument.
+
+        Args:
+            arg (str): The name of the class to create an object from.
+
+        Returns:
+            None
+
+        Raises:
+            None
+        """
+        if HBNBCommand.__checkMissingClassName(arg):
+            return
+
+        objClass = HBNBCommand.__get_class(arg)
+        if not objClass:
+            return
+
+        obj = objClass()
+        obj.save()
+        print("{}".format(obj.id))
+
+    def do_all(self, arg):
+        pass
+
+    def do_show(self, arg):
+        if HBNBCommand.__checkMissingClassName(arg):
+            return
+
+        if not HBNBCommand.__get_class(arg):
+            return
+
+        if HBNBCommand.__checkMissingClassID(arg):
+            return
+
+        objInstance = HBNBCommand.__get_classDetails(arg)
+
+        if not objInstance:
+            return
+
+        print(objInstance)
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
